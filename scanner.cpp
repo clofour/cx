6ab4ebcf83262e058176704e0c5e207bac264d78
clofour <208706398+clofour@users.noreply.github.com>
2026-06-14 22:25:51 +0200

    Bug fixes of various types for scanner

--- scanner.cpp
+++ scanner.cpp
@@ -2,22 +2,24 @@
 
 typedef struct {
     char* source;
+    int line;
     int start;
     int current;
     Token* tokens;
 } Scanner;
 
-char advance(Scanner scanner) {
-    return scanner.source[scanner.current++];
+char advance(Scanner *scanner) {
+    return scanner->source[scanner->current++];
 }
 
-Token* add_token(Scanner scanner, TokenType type) {
+Token* add_token(Scanner *scanner, TokenType type) {
     Token token;
     token.TokenType = type;
-    token.start = &(scanner.source[scanner.start]);
+    token.line = scanner->line;
+    token.start = &(scanner->source[scanner->start]);
 }
 
-Token* scan_token(Scanner scanner) {
+Token* scan_token(Scanner *scanner) {
     char character = advance(scanner);
     switch (character) {
         case '(': return add_token(scanner, TOKEN_LEFT_PARENTHESIS);
@@ -29,7 +31,7 @@ Token* scan_source(char* source) {
     scanner.source = source;
     scanner.start = scanner.current;
 
-    scan_token(scanner);
+    scan_token(&scanner);
 
     return scanner.tokens;
 }
