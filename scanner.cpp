5deda8ede22f731bdceba930af2c544a50dbc274
clofour <208706398+clofour@users.noreply.github.com>
2026-06-14 22:27:19 +0200

    Add rest of single character tokens

--- scanner.cpp
+++ scanner.cpp
@@ -23,6 +23,16 @@ Token* scan_token(Scanner *scanner) {
     char character = advance(scanner);
     switch (character) {
         case '(': return add_token(scanner, TOKEN_LEFT_PARENTHESIS);
+        case ')': return add_token(scanner, TOKEN_RIGHT_PARENTHESIS);
+        case '{': return add_token(scanner, TOKEN_LEFT_BRACE);
+        case '}': return add_token(scanner, TOKEN_RIGHT_BRACE);
+        case ',': return add_token(scanner, TOKEN_COMMA);
+        case '.': return add_token(scanner, TOKEN_DOT);
+        case '-': return add_token(scanner, TOKEN_MINUS);
+        case '+': return add_token(scanner, TOKEN_PLUS);
+        case ';': return add_token(scanner, TOKEN_SEMICOLON);
+        case '/': return add_token(scanner, TOKEN_SLASH);
+        case '*': return add_token(scanner, TOKEN_STAR);
     }
 }
 
