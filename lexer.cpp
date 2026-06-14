d3cf3662990c82e0f8b979708450fc15981c846d
clofour <208706398+clofour@users.noreply.github.com>
2026-06-14 21:44:56 +0200

    Implement types and skeleton for lexer
    Resources:
    * https://craftinginterpreters.com/scanning.html

---
+++ lexer.cpp
@@ -0,0 +1,5 @@
+#include "lexer.h"
+
+Token* scan(char* source) {
+    
+}
---
+++ lexer.h
@@ -0,0 +1,68 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+enum TokenType
+{
+    TOKEN_LEFT_PARENTHESIS,
+    TOKEN_RIGHT_PARENTHESIS,
+    TOKEN_LEFT_BRACE,
+    TOKEN_RIGHT_BRACE,
+    TOKEN_COMMA,
+    TOKEN_DOT,
+    TOKEN_MINUS,
+    TOKEN_PLUS,
+    TOKEN_SEMICOLON,
+    TOKEN_SLASH,
+    TOKEN_STAR,
+
+    TOKEN_BANG,
+    TOKEN_BANG_EQUAL,
+    TOKEN_EQUAL,
+    TOKEN_EQUAL_EQUAL,
+    TOKEN_GREATER,
+    TOKEN_GREATER_EQUAL,
+    TOKEN_LESS,
+    TOKEN_LESS_EQUAL,
+
+    TOKEN_IDENTIFIER,
+    TOKEN_STRING,
+    TOKEN_NUMBER,
+
+    TOKEN_AND,
+    TOKEN_CLASS,
+    TOKEN_ELSE,
+    TOKEN_FALSE,
+    TOKEN_FUNCTION,
+    TOKEN_FOR,
+    TOKEN_IF,
+    TOKEN_NULL,
+    TOKEN_OR,
+    TOKEN_PRINT,
+    TOKEN_RETURN,
+    TOKEN_TRUE,
+    TOKEN_VAR,
+    TOKEN_WHILE,
+
+    TOKEN_EOF
+};
+
+typedef struct
+{
+    TokenType TokenType;
+
+    char *start;
+    int length;
+    int line;
+    int column;
+
+    union
+    {
+        long int_value;
+        double float_value;
+        char *string_value;
+    } value;
+} Token;
+
+Token *scan(char *source);
+
+#endif
