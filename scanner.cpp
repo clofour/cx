be6f0fc938af8c44f5929c3676282270834c7716
clofour <208706398+clofour@users.noreply.github.com>
2026-06-14 22:21:18 +0200

    Develop scanner skeleton

--- scanner.cpp
+++ scanner.cpp
@@ -1,5 +1,35 @@
 #include "scanner.h"
 
-Token* scan(char* source) {
-    
+typedef struct {
+    char* source;
+    int start;
+    int current;
+    Token* tokens;
+} Scanner;
+
+char advance(Scanner scanner) {
+    return scanner.source[scanner.current++];
+}
+
+Token* add_token(Scanner scanner, TokenType type) {
+    Token token;
+    token.TokenType = type;
+    token.start = &(scanner.source[scanner.start]);
+}
+
+Token* scan_token(Scanner scanner) {
+    char character = advance(scanner);
+    switch (character) {
+        case '(': return add_token(scanner, TOKEN_LEFT_PARENTHESIS);
+    }
+}
+
+Token* scan_source(char* source) {
+    Scanner scanner;
+    scanner.source = source;
+    scanner.start = scanner.current;
+
+    scan_token(scanner);
+
+    return scanner.tokens;
 }
