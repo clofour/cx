86268da1125428e7380d91d6b157a48ef9e75c67
clofour <208706398+clofour@users.noreply.github.com>
2026-06-21 22:56:24 +0200

    Add missing declarations to dynamic buffer header file

--- dynamic_buffer.h
+++ dynamic_buffer.h
@@ -7,4 +7,10 @@ typedef struct {
     char* buffer;
 } DynamicBuffer;
 
+DynamicBuffer create_dynamic_buffer(int init_capacity);
+void resize_dynamic_buffer(DynamicBuffer* dynamic_buffer, int new_capacity);
+void grow_dynamic_buffer(DynamicBuffer* dynamic_buffer);
+void append_dynamic_buffer(DynamicBuffer* dynamic_buffer, char* append_buffer, int length);
+void free_dynamic_buffer(DynamicBuffer* dynamic_buffer);
+
 #endif
