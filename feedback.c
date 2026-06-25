71cb1f2458b68c99a0e9588c67a86d3994592970
clofour <208706398+clofour@users.noreply.github.com>
2026-06-25 12:51:18 +0200

    Colored errors!

--- feedback.c
+++ feedback.c
@@ -1,7 +1,9 @@
+#include "feedback.h"
 #include "scanner.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdbool.h>
 
 void feedback(char* format, ...) {
     va_list args;
@@ -14,14 +16,36 @@ void feedback(char* format, ...) {
     exit(EXIT_FAILURE);
 }
 
+void color(Color color, bool state) {
+
+    if (state == true) {
+        switch (color) {
+            case COLOR_RED:
+                printf("%s", "\033[31m");
+        }
+    } else {
+        switch (color) {
+            case COLOR_RED:
+                printf("%s", "\033[0m");
+        }
+    }
+
+}
+
 void error(char* message) {
+    color(COLOR_RED, true);
     feedback("%s: error: %s", "hello", message);
+    color(COLOR_RED, false);
 }
 
 void error_token(Token* token, char* message) {
+    color(COLOR_RED, true);
     feedback("%s:%d: error: %s\n", "hello", token->line, message);
+    color(COLOR_RED, false);
 }
 
 void error_line(int line, char* message) {
+    color(COLOR_RED, true);
     feedback("%s:%d: error: %s\n", "hello", line, message);
+    color(COLOR_RED, false);
 }
--- feedback.h
+++ feedback.h
@@ -1,6 +1,12 @@
 #ifndef FEEDBACK_H
 #define FEEDBACK_H
 
+#include "scanner.h"
+
+typedef enum {
+    COLOR_RED,
+} Color;
+
 void error(char* message);
 void error_token(Token* token, char* message);
 void error_line(int line, char* message);
